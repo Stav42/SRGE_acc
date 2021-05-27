8224122270ad0a0cfdbd4a56464f67004011314d
Stav42 <vaditya@iitk.ac.in>
2021-05-27 21:24:57 +0530

    adding flow animations

--- granular_ellipsoid/granular_ellipsoid.m
+++ granular_ellipsoid/granular_ellipsoid.m
@@ -19,15 +19,21 @@ hold on
 %visible.
 %fsurf(z2)
 
+%no of random points
+n_o_rp = 5000;
 
 %picking random points
-x_rnd = -(a-xc) + 2*a*rand(1,5000);
-y_rnd = -(b-yc) + 2*b*rand(1,5000);
-z_rnd = -(c-zc) + 2*c*rand(1,5000);
+x_rnd = -(a-xc) + 2*a*rand(1,n_o_rp);
+y_rnd = -(b-yc) + 2*b*rand(1,n_o_rp);
+z_rnd = -(c-zc) + 2*c*rand(1,n_o_rp);
+
+%mean_size of grains
+mean_size = 36;
 
 %acquire random size distribution for grains around mean 36. distribution
 %is +-10.
-rnd_size = 36 + 2*5*rand(1,5000);
+spread = 10;
+rnd_size = mean_size + spread*rand(1,n_o_rp);
 
 %function defining the ellipsoid
 F1 = ((x_rnd-xc)/a).^2 + ((y_rnd-yc)/b).^2 + ((z_rnd-zc)/c).^2 - 1;
